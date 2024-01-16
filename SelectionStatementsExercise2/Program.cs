0bd67cc3ae42b96e0deef06e480d7f4bf2f23da7
JustinBenning <justin.benning@gmail.com>
2024-01-16 10:22:28 -0500

    favorite subject

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject in school?");
+            string userInput = Console.ReadLine();
+            string favSubject = userInput.ToLower();
+            switch (favSubject)
+            {
+                case "english":
+                    Console.WriteLine("You like books.");
+                    break;
+                case "science":
+                    Console.WriteLine("I think we have chemistry");
+                    break;
+                case "history":
+                    Console.WriteLine("Four score and seven years ago");
+                    break;
+                case "math":
+                    Console.WriteLine("Nerd");
+                    break;
+                default:
+                    Console.WriteLine("Slacker");
+                    break;
+            }   
         }
     }
 }
